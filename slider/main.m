1ce726091dbc742e731c9dbf75cfc937b55dbafa
Dereck Lesko <lesko@moravian.edu>
2014-01-27 14:35:34 -0500

    Initial Commit

---
+++ slider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  slider
+//
+//  Created by Lesko, Dereck on 1/27/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "sliderAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([sliderAppDelegate class]));
+    }
+}
---
+++ slider/sliderViewController.h
@@ -0,0 +1,13 @@
+//
+//  sliderViewController.h
+//  slider
+//
+//  Created by Lesko, Dereck on 1/27/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sliderViewController : UIViewController
+
+@end
